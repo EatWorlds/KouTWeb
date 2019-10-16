e5c150f4de0be12c449cc96c5e4b6b86e8526a43
Dimle <LittleDimple91@gmail.com>
2019-10-16 22:16:54 +0800

    1:新增跨域解决

---
+++ BackEnd/src/main/java/com/cutout/server/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.cutout.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * https://blog.csdn.net/qq_28340701/article/details/89521387
+ */
+// 全局跨域配置
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer  {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域访问
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*")
+                // 是否允许证书 不再默认开启
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+}
